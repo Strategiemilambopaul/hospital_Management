77d6792b1dd0ba738db1d4758c66ae32eb5d9718
strat mil's <pmilambo47@gmail.com>
2024-06-10 11:37:22 +0100

    first commitP

---
+++ hms/admin/include/config.php
@@ -0,0 +1,19 @@
+<?php
+  $serveur = "localhost";
+  $password ="";
+  $database ="hms";
+  $username="root";
+  
+  
+  
+  try {
+  
+      $con= new PDO("mysql:host=$serveur;dbname=$database", "$username", "$password");  
+
+  
+  
+  } catch (Error $e) {
+      echo "Failed to connect to Mysql". $e->getMessage(); 
+  }
+  
+?>
